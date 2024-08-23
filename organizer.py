71d231c419470a3fb40eb4cb4df8911c31054658
ZaguePrime <kivashin47@gmail.com>
2024-08-23 22:35:49 +0200

    feat: minimal updates

--- organizer.py
+++ organizer.py
@@ -8,8 +8,8 @@ import json
 
 working_directory = ""
 organizer_configuration = {}
-
-
+organized_paths = {}
+current_files = {}
 #reads the user configuration for organizer
 def get_config():
     #get the global variable
@@ -19,10 +19,15 @@ def get_config():
         #store the json contents in a dictionary
         organizer_configuration = json.load(open_file)
 
-
+#opens the directory to work in and stores its path
 def open_directory():
+    #get the global variable
     global working_directory
+    #ask for the directory
     fp = fd.askdirectory()
+    #if not null just assign the value to the variable
     if fp:
         working_directory = fp
+
+
     
