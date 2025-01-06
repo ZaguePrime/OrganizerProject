8555cc6f003f9fdede3f4021f40d5ac76fda6454
Kivashin Naidoo <kivashin47@gmail.com>
2025-01-06 23:15:24 +0200

    New idea for remove element

--- src/components/preferencesInput.tsx
+++ src/components/preferencesInput.tsx
@@ -2,19 +2,23 @@ import '../preferencesInput.css';
 // interface PreferencesInputProps {
 
 // }
+import SubtractButton from './subtractButton';
 
 const preferencesInput = () => {
 
     return (
-        <div className = "container d-inline-flex justify-content-center bg-secondary">
-            <div className = "row gx-0">
+        <div key='' className = "container d-inline-flex justify-content-center bg-secondary">
+            <div className = "row gx-5">
                 <div className = "col">
-                    <label className="form-label d-flex justify-content-center">Folder Name</label>
+                    <label className="form-label d-flex justify-content-center">Folder</label>
                     <input type="text" className="form-control foldername-input"></input>
                 </div>
                 <div className = "col">
-                    <label className="form-label d-flex justify-content-center">Regex or Text to match</label>
+                    <label className="form-label d-flex justify-content-center">Match</label>
                     <input type="text" className="form-control regex-text"></input>
+                </div>
+                <div className = "col d-flex align-items-center">
+                    <SubtractButton buttonClicked={() => {}}/>
                 </div>
             </div>
         </div>
